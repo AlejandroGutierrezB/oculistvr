01d854c9b3ab04438232cac8e5344f115df85ca9
Alejandro Gutierrez <alejandrogutbarce@gmail.com>
2021-09-11 13:52:51 +0100

    (feat) get game image

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer');
 const fs = require('fs').promises;
 require('dotenv').config();
+
+const getDate = require('./helpers/getDate');
 
 (async (event, context) => {
   const browser = await puppeteer.launch({
@@ -30,7 +31,6 @@ require('dotenv').config();
   // await autoScroll(page);
   const results = await page.$$eval('.section__items-cell', (games) => {
     return games.map((game) => {
-      console.log('🚀 ~ file: meta.js ~ line 20 ~ results ~ game', game);
       let [title, price] = game.innerText.split('\n');
 
       const priceWithoutCurrency = price.split(' ')[0];
@@ -39,15 +39,27 @@ require('dotenv').config();
 
       const finalHref = game.innerHTML.match(/href="(.*?)"/)[1];
 
+      const [trash, ...baseImageUrl] = game.innerHTML
+        .match(/style="(.*?)"/)[1]
+        .split(';');
+      const imageUrl = baseImageUrl
+        .join('')
+        .replace(/amp/g, '')
+        .split('&quot')[0];
+
       return {
         title,
         price,
         href: `https://www.oculus.com${finalHref}`,
-        image: game.href, //!
+        image: imageUrl,
       };
     });
   });
   console.log('🚀 ~ file: index.js ~ line 48 ~ results ~ results', results);
+  await fs.writeFile(
+    `./data/${getDate()}.json`,
+    JSON.stringify(results, null, 2)
+  );
 
   //grid
   // '/html/body/div/div[1]/div[3]/div[3]/div/div[2]/div/div/div[2]'
@@ -62,10 +74,10 @@ require('dotenv').config();
 const autoScroll = async (page) => {
   await page.evaluate(async () => {
     await new Promise((resolve, reject) => {
-      var totalHeight = 0;
-      var distance = 100;
-      var timer = setInterval(() => {
-        var scrollHeight = document.body.scrollHeight;
+      const totalHeight = 0;
+      const distance = 100;
+      const timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight;
         window.scrollBy(0, distance);
         totalHeight += distance;
 
